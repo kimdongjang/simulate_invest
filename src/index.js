bfb9efbe2163a3b73cbf96b56d3c536ab50648ab
farrar142 <gksdjf1690@gmail.com>
2021-11-16 15:44:34 +0900

    마우스호버링기능추가

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Layout from './MainChart/Layout';
-import ChartComponent from './MainChart/ChartComponent';
 
 ReactDOM.render(
 	<Layout />, document.getElementById("root"),
