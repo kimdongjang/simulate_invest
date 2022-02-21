ee6c9953262f79d1d13b09c7f25965479c2cac5b
JeonWoonKi <naru_oo@naver.com>
2022-02-21 22:51:56 +0900

    웹소켓 미들웨어 작업중

--- src/redux/SocketSaga.jsx
+++ src/redux/SocketSaga.jsx
@@ -1,51 +1,28 @@
-import { call, put, select, flush, delay } from "redux-saga/effects";
-import { createSocket } from './Socket';
-import { connectSocket } from './CreateSocketChannel';
-import { buffers } from 'redux-saga';
+import { call, put, take, takeEvery } from 'redux-saga/effects';
+import ws from './socket';
+import { closeChannel, createSocketChannel } from './createSocketChannel';
+import { actions, types } from './actions/SocketAction';
 
-export const createConnectSocketSaga = (type, connectType, dataMaker) => {
-    const SUCCESS = `${type}_SUCCESS`;
-    const ERROR = `${type}_ERROR`;
-
-    return function* (action = {}) {
-        const client = yield call(createSocket);
-        const clientChannel = yield call(
-            connectSocket,
-            client,
-            connectType,
-            action,
-            buffers.expanding(500)
-        );
-
+function* waitTask() {
+    let channel;
+    try {
+        channel = yield call(createSocketChannel, "tasks");
         while (true) {
-            try {
-                const datas = yield flush(clientChannel); // 버퍼 데이터 가져오기
-                const state = yield select();
-
-                if (datas.length) {
-                    const sortedObj = {};
-                    datas.forEach((data) => {
-                        if (sortedObj[data.code]) {
-                            // 버퍼에 있는 데이터중 시간이 가장 최근인 데이터만 남김
-                            sortedObj[data.code] =
-                                sortedObj[data.code].timestamp > data.timestamp
-                                    ? sortedObj[data.code]
-                                    : data;
-                        } else {
-                            sortedObj[data.code] = data; // 새로운 데이터면 그냥 넣음
-                        }
-                    });
-
-                    const sortedData = Object.keys(sortedObj).map(
-                        (data) => sortedObj[data]
-                    );
-
-                    yield put({ type: SUCCESS, payload: dataMaker(sortedData, state) });
-                }
-                yield delay(500); // 500ms 동안 대기
-            } catch (e) {
-                yield put({ type: ERROR, payload: e });
-            }
+            const task = yield take(channel);
+            yield onTask(task);
         }
-    };
-};
+    } catch (e) {
+        console.log(e, "error");
+    } finally {
+        ws.current.onclose();
+        closeChannel(channel);
+    }
+}
+
+function* onTask(task) {
+    yield put(actions.pushTask(task));
+}
+
+export default function* watcher() {
+    yield takeEvery(types.WAIT_TASK, waitTask);
+}
