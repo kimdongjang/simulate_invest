c614f0b03a71569ccda61f939951bcf3543e82ef
JeonWoonKi <naru_oo@naver.com>
2022-05-02 22:36:40 +0900

    saga 작업중

--- simulate_app/redux/Store.tsx
+++ simulate_app/redux/Store.tsx
@@ -1,36 +1,48 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import { createWrapper, MakeStore } from "next-redux-wrapper";
-import createSagaMiddleware from "redux-saga";
-import { createLogger } from "redux-logger";
+import { createStore, applyMiddleware, Store } from 'redux';
+import { createWrapper, Context } from 'next-redux-wrapper';
+import createSagaMiddleware, { Task } from 'redux-saga';
+import reducer, { State } from './reducer';
+import rootSaga from './saga';
 
-import RootReducer from "./reducers/RootReducer";
-import RootSaga from "./saga/RootSaga";
 
-// export const makeStore = () => {
-//     const sagaMiddleware = createSagaMiddleware();
-//     const logger = createLogger();
-//     const store = createStore(RootReducer, composeWithDevTools(applyMiddleware(sagaMiddleware, logger)));
-//     // sagaMiddleware.run(WatcherSaga);
+export interface SagaStore extends Store {
+  sagaTask?: Task;
+}
 
-//     return store;
+export const makeStore = (context: Context) => {
+  // 1: Create the middleware
+  const sagaMiddleware = createSagaMiddleware();
+
+  // 2: Add an extra parameter for applying middleware:
+  const store = createStore(reducer, applyMiddleware(sagaMiddleware));
+
+  // 3: Run your sagas on server
+  (store as SagaStore).sagaTask = sagaMiddleware.run(rootSaga);
+
+  // 4: now return the store:
+  return store;
+};
+
+export const wrapper = createWrapper<Store<State>>(makeStore, { debug: true });
+
+
+// const configureStore = () => {
+//   // 사가용 미들웨어 생성
+//   const sagaMiddleware = createSagaMiddleware();
+//   const middlewares = [sagaMiddleware];
+
+
+//   // production일 경우 확인
+//   const enhancer = process.env.NODE_ENV === 'production'
+//     ? compose(applyMiddleware(...middlewares))
+//     : composeWithDevTools(
+//       applyMiddleware(...middlewares),
+//     );
+//   const store = createStore(RootReducer, enhancer);
+//   store.sagaTask = sagaMiddleware.run(RootSaga);
+//   return store;
 // };
 
-// export const wrapper = createWrapper(makeStore, { debug: true })
+// const wrapper = createWrapper(configureStore, { debug: process.env.NODE_ENV === 'development' });
 
-const configureStore = () => {
-    const sagaMiddleware = createSagaMiddleware();
-    const middlewares = [sagaMiddleware];
-    const enhancer = process.env.NODE_ENV === 'production'
-      ? compose(applyMiddleware(...middlewares))
-      : composeWithDevTools(
-        applyMiddleware(...middlewares),
-      );
-    const store = createStore(RootReducer, enhancer);
-    store.sagaTask = sagaMiddleware.run(RootSaga);
-    return store;
-  };
-  
-  const wrapper = createWrapper(configureStore, { debug: process.env.NODE_ENV === 'development' });
-  
-  export default wrapper;
+// export default wrapper;
